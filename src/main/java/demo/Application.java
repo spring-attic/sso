d07e4c488c4026c17ab8de59ec8aa33112688f67
Dave Syer <dsyer@pivotal.io>
2014-11-05 09:27:52 +0000

    Initial working version

---
+++ src/main/java/demo/Application.java
@@ -0,0 +1,38 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.security.sso.EnableOAuth2Sso;
+import org.springframework.cloud.security.sso.OAuth2SsoConfigurerAdapter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@RestController
+@EnableOAuth2Sso
+public class Application {
+
+	@RequestMapping("/dashboard")
+	public String dashboard() {
+		return "<html><body>Dashboard!</body></html>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Component
+	public static class LoginConfigurer extends OAuth2SsoConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/dashboard/**").authorizeRequests().anyRequest()
+					.authenticated();
+		}
+	}
+}

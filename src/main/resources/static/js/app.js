4cebf58059c38bac3d44cc9282de702d8472cae8
Dave Syer <dsyer@pivotal.io>
2014-11-11 07:31:04 +0000

    Add angular layer using cookies for backend auth

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,39 @@
+angular.module('sso', [ 'ngRoute', 'ngResource', 'ngCookies' ]).config(
+		function($routeProvider) {
+
+			$routeProvider.otherwise('/');
+			$routeProvider.when('/', {
+				templateUrl : 'home.html',
+				controller : 'home'
+			}).when('/dashboard', {
+				templateUrl : 'dashboard.html',
+				controller : 'dashboard'
+			});
+
+		}).controller('navigation', function($scope, $http, $window) {
+	if (!$scope.user) {
+		$http.get('/dashboard/user').success(function(data) {
+			$scope.user = data;
+			$scope.authenticated = true;
+		}).error(function() {
+			$scope.authenticated = false;
+		});
+	}
+	$scope.logout = function() {
+		$http.post('/dashboard/logout', {}).success(function() {
+			delete $scope.user;
+			$scope.authenticated = false;
+			// Force reload of home page to reset all state after logout
+			$window.location.hash = '';
+		});
+	};
+}).controller('home', function() {
+}).controller('dashboard', function($scope, $resource) {
+
+	$resource('/dashboard/message', {}).get({}, function(data) {
+		$scope.message = data.message;
+	}, function() {
+		$scope.message = '';
+	});
+
+});

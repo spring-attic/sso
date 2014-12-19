9fe0149f5bda4c47560d6c969cda29f49d602624
Dave Syer <dsyer@pivotal.io>
2014-12-19 16:44:35 +0000

    Fix integration test by providing JWT verifier key
    The default is to get the key from the auth server, but if
    that isn't running we fail on startup.
    
    Fixes gh-1

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -3,12 +3,14 @@ package demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SsoApplication.class)
 @WebAppConfiguration
+@IntegrationTest({"server.port=0", "oauth2.resource.jwt.keyValue=DEADBEEF"})
 public class ApplicationTests {
 
 	@Test

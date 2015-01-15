95c3055edc5b513a7d563bb49ff7ee90137a4a42
Dave Syer <dsyer@pivotal.io>
2015-01-15 13:16:27 +0000

    Add some tests

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -1,20 +1,67 @@
 package demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SsoApplication.class)
 @WebAppConfiguration
-@IntegrationTest({"server.port=0", "spring.oauth2.resource.jwt.keyValue=DEADBEEF"})
+@IntegrationTest("server.port:0")
 public class ApplicationTests {
 
+	@Value("${local.server.port}")
+	private int port;
+
+	@Value("${spring.oauth2.client.userAuthorizationUri}")
+	private String authorizeUri;
+
+	private RestTemplate template = new TestRestTemplate();
+
 	@Test
-	public void contextLoads() {
+	public void homePageLoads() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
+	@Test
+	public void userEndpointProtected() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/dashboard/user", String.class);
+		assertEquals(HttpStatus.FOUND, response.getStatusCode());
+		String location = response.getHeaders().getFirst("Location");
+		assertTrue("Wrong location: " + location,
+				location.startsWith("http://localhost:" + port + "/dashboard/login"));
+	}
+
+	@Test
+	public void envEndpointProtected() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/env", String.class);
+		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+		assertEquals("Basic realm=\"Spring\"",
+				response.getHeaders().getFirst("WWW-Authenticate"));
+	}
+
+	@Test
+	public void loginRedirects() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/dashboard/login", String.class);
+		assertEquals(HttpStatus.FOUND, response.getStatusCode());
+		String location = response.getHeaders().getFirst("Location");
+		assertTrue("Wrong location: " + location, location.startsWith(authorizeUri));
 	}
 
 }
